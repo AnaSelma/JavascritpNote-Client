5dc6243d28772369e02620a744e99aa4543e35f1
AnaSelma <anaselma1962@gmail.com>
2021-09-25 10:18:28 -0300

    creating the project structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import "./App.scss";
+import { Notification, Section } from 'rbx';
 
-function App() {
-  return (
-    <div>
-      <p>
-          Javascript Notes!
-      </p>
-        
-    </div>
-  );
-}
+const App = () => (
+  <Fragment>
+    <Section>
+      <Notification color="success">
+        Javascrit Notes!
+      </Notification>
+    </Section>
+  </Fragment>
+)
 
 export default App;
