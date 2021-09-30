0a93548700a9ef857420aac938d709a48dab4834
AnaSelma <anaselma1962@gmail.com>
2021-09-30 19:45:01 -0300

    create login screen and connect an API

--- src/screens/auth/login/index.js
+++ src/screens/auth/login/index.js
@@ -1,8 +1,41 @@
 import React, { Fragment } from 'react';
+import Header from '../../../components/header';
+import LoginForm from '../../../components/auth/login_form';
+import { Column, Section, Container, Card } from 'rbx';
+import LogoImage from '../../../assets/images/logo.png';
+import "../../../style/auth.scss";
 
 const LoginScreen = () => (
   <Fragment>
-    Hello World!
+    <Header />
+    <Section size="medium" className="auth">
+      <Container>
+        <Column.Group centered>
+          <Column size={3}>
+            <Card>
+              <Card.Content>
+                <Section>
+                  <Column.Group centered>
+                    <Column size={12}>
+                      <img src={LogoImage} alt="Logo"/>
+                    </Column>
+                  </Column.Group>
+                </Section>
+                <Section>
+                  <Column.Group centered>
+                    <Column size={6} className="has-text-grey has-text-centered">
+                      Your notes on the cloud
+                    </Column>
+                  </Column.Group>
+                </Section>
+                <LoginForm />
+              </Card.Content>
+            </Card>
+          </Column>
+        </Column.Group>
+      </Container>
+    </Section>
+
   </Fragment>
 );
 
