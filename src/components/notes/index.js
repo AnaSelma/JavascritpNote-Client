10445c78ba706123b8a5498595e9f9d1d98880d1
AnaSelma <anaselma1962@gmail.com>
2021-10-06 18:39:12 -0300

    create and show notes listings

--- src/components/notes/index.js
+++ src/components/notes/index.js
@@ -1,13 +1,36 @@
 import React, { Fragment, useState, useEffect } from 'react';
-import { Column, Button } from 'rbx';
+import { Column } from 'rbx';
+import List from '../notes/list';
+import NoteService from "../../services/note";
 import "../../style/notes.scss";
 import { push as Menu } from 'react-burger-menu';
 
 function Notes(props) {
-  
+  const [notes, setNotes] = useState([]);
+  const [current_note, setCurrentNote] = useState({ title: "", body: "", id: "" });
+
+  async function fetchNotes() {
+    const response = await NoteService.index();
+    if (response.data.length >= 1) {
+      setNotes(response.data.reverse())
+      setCurrentNote(response.data[0])
+    }
+  }
+
+  const selectNote = (id) => {
+    const note = notes.find((note) => {
+      return note._id === id;
+    })
+    setCurrentNote(note);
+  }
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
   return (
     <Fragment>
-      <Column.Group className="notes" id="notes">
+      <Column.Group className="notes" >
         <Menu
           pageWrapId={"notes-editor"}
           isOpen={props.isOpen}
@@ -17,15 +40,18 @@ function Notes(props) {
           customBurgerIcon={false}
           customCrossIcon={false}
         >
-          <Column.Group>
+           <Column.Group>
             <Column size={10} offset={1}>
               Search...
             </Column>
           </Column.Group>
-          <p>List...</p>
+          <List
+            notes={notes}
+            selectNote={selectNote}
+            current_note={current_note} />
         </Menu>
 
-        <Column size={12} className="notes-editor" id="notes-editor">
+        <Column size={12} className="notes-editor" >
           Editor...
         </Column>
       </Column.Group>
